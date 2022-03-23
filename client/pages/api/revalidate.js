0bb174c592f8c8296617b1e4ecbbaa668ba00709
Mahmoud Salah <mahmoudsalah11350@gmail.com>
2022-03-23 20:33:55 +0200

    revalidate

--- client/pages/api/revalidate.js
+++ client/pages/api/revalidate.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   try {
-    await res.unstable_revalidate('/product:id')
+    await res.unstable_revalidate('/product/')
     return res.json({ revalidated: true })
   } catch (err) {
     return res.status(500).send('Error revalidating')
